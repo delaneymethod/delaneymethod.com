dbae1ff1cee589590f598ff2bb70581744c7c112
Sean Delaney <hello@delaneymethod.com>
2016-11-26 23:03:04 +0000

    Base Craft Config

--- craft/config/general.php
+++ craft/config/general.php
@@ -8,5 +8,54 @@
  */
 
 return array(
-
+	'*' => array(
+		// general options
+		'defaultWeekStartDay' => 1,
+		'sendPoweredByHeader' => false,
+		// urls
+		'addTrailingSlashesToUrls' => true,
+		'omitScriptNameInUrls' => true,
+		'defaultCpLanguage' => 'en_ie',
+		// user options
+		'useEmailAsUsername' => true,
+	    // template options
+		'errorTemplatePrefix' => '_errors/',
+		'environmentVariables' => array(
+			'baseAssetsUrl' => '//'.strtolower($_SERVER['SERVER_NAME']).'/assets/',
+		),
+	),
+    '.local' => array(
+	    'appId' => 'development',
+	    'siteUrl' => 'http://www.delaneymethod.local',
+	    'devMode' => true,
+	    'translationDebugOutput' => false,
+	    'useCompressedJs' => false,
+        // caching options
+		'enableTemplateCaching' => false,
+        'environmentVariables' => array(
+			'baseAssetsPath' => realpath(dirname(__FILE__)).'/../../html/assets/'
+		),
+		// user options
+		'testToEmailAddress' => 'hello@delaneymethod.com',
+	),
+    '.com' => array(
+	    'appId' => 'production',
+	    'siteUrl' => 'http://www.delaneymethod.com',
+	    'useCompressedJs' => true,
+        'cooldownDuration' => 0,
+        'environmentVariables' => array(
+	    	'baseAssetsPath' => realpath(dirname(__FILE__)).'/../../html/assets/'
+		),
+		// updates
+		'allowAutoUpdates' => false,
+		'backupDbOnUpdate' => false,
+    	'restoreDbOnUpdateFailure' => false,
+    	// security options
+		'enableCsrfProtection' => true,
+		'validationKey' => 'BCCDEE2B724DD98F1E61FE08936A570FFF30C146BC20FB2B044DB37DBA12E664',
+		// caching
+		// 'cacheMethod' => 'file',
+		// assets
+		'imageDriver' => 'imagick',
+    ),
 );
